2201c73ed5a1f801c16f0759e5c2e6f2d0cd7bf9
Emmanuel Etti <etti.emman@gmail.com>
2020-08-06 17:24:51 -0300

    updated file download process

--- web-app/server/src/app.js
+++ web-app/server/src/app.js
@@ -4,22 +4,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const fs = require('fs');
 
 let network = require('./fabric/network.js');
 
 const app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
 
+const host = process.env.Host || 'http://localhost';
+const port = process.env.PORT || 8081;
+
+const url = host + ':' + port;
+
 //get user info, create user object, and update state with their emailAddress
-app.post('/registerUser', async (req, res) => {
+app.post('/registerUser', async(req, res) => {
     //first create the identity for the user and add to wallet
     let response = await network.registerUser(req.body.emailAddress, req.body.firstName, req.body.lastName);
     if (response.err) {
         res.send(response.err);
-    }
-    else {
+    } else {
         //connect to the network as the newly registered user
         let networkObj = await network.connectToNetwork(req.body.emailAddress);
 
@@ -35,15 +40,14 @@ app.post('/registerUser', async (req, res) => {
 
         if (invokeResponse.err) {
             res.send(invokeResponse.err);
-        }
-        else {
+        } else {
             let registerResponse = JSON.parse(invokeResponse).data + ' Use emailAddress to login above.';
             res.send(registerResponse);
         }
     }
 });
 
-app.post('/validateUser', async (req, res) => {
+app.post('/validateUser', async(req, res) => {
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     if (networkObj.err) {
         res.send(networkObj);
@@ -51,15 +55,14 @@ app.post('/validateUser', async (req, res) => {
     let invokeResponse = await network.queryAllDigitalAssets(networkObj, req.body.emailAddress);
     if (invokeResponse.err) {
         res.send(invokeResponse);
-    }
-    else {
+    } else {
         let parsedResponse = await JSON.parse(invokeResponse);
         res.send(parsedResponse);
     }
 });
 
 //get all assets in world state
-app.post('/queryAllDigitalAssets', async (req, res) => {
+app.post('/queryAllDigitalAssets', async(req, res) => {
 
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     let response = await network.queryAllDigitalAssets(networkObj, req.body.emailAddress);
@@ -69,7 +72,7 @@ app.post('/queryAllDigitalAssets', async (req, res) => {
 });
 
 //get all assets owned by a particular user
-app.post('/queryDigitalAssetsByUser', async (req, res) => {
+app.post('/queryDigitalAssetsByUser', async(req, res) => {
 
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     let response = await network.queryDigitalAssetsByUser(networkObj, req.body.emailAddress);
@@ -79,7 +82,7 @@ app.post('/queryDigitalAssetsByUser', async (req, res) => {
 });
 
 //get asset by assetId
-app.post('/readDigitalAsset', async (req, res) => {
+app.post('/readDigitalAsset', async(req, res) => {
 
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     let response = await network.readDigitalAsset(networkObj, req.body.assetId);
@@ -89,7 +92,7 @@ app.post('/readDigitalAsset', async (req, res) => {
 });
 
 //create/upload a new asset
-app.post('/createDigitalAsset', async (req, res) => {
+app.post('/createDigitalAsset', async(req, res) => {
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     let response = await network.createDigitalAsset(networkObj, req.body.digitalAssetFileName, req.body.digitalAssetFileType, req.body.digitalAssetFileBuffer, req.body.emailAddress);
     res.send(response);
@@ -97,47 +100,69 @@ app.post('/createDigitalAsset', async (req, res) => {
 });
 
 //view all pending asset modification requests
-app.post('/viewAssetModificationRequests', async (req, res) => {
+app.post('/viewAssetModificationRequests', async(req, res) => {
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     let response = await network.viewAssetModificationRequests(networkObj, req.body.emailAddress);
     res.send(response.toString());
 });
 
 //download an asset from COS
-app.post('/downloadDigitalAssetFile', async (req, res) => {
+app.post('/downloadDigitalAssetFile', async(req, res) => {
     let response = await network.downloadDigitalAssetFile(req.body.assetId, req.body.assetName);
-    res.send(response);
+
+    let buffer = Buffer.from(response.Body).toString();
+
+    let base64Data = buffer.replace(/^data:image\/(png|gif|jpeg);base64,/, '');
+    const file = __dirname + '/files/' + req.body.assetName;
+    require('fs').writeFile(file, base64Data, 'base64', function(err) {
+        console.log(err);
+    });
+
+    let link = req.body.assetName;
+    let finalRes = {};
+    finalRes.link = `${url}/download/${link}`;
+
+    // res.setHeader('Content-disposition', `attachment; filename=${req.body.assetName}`);
+    // res.setHeader('Content-type', 'image/jpeg');
+    res.send(finalRes);
+    // res.send(response);
+});
+
+app.get('/download/:id', async(req, res) => {
+    const id = req.params.id;
+    const file = `${__dirname}/files/${id}`;
+    res.download(file);
 });
 
 //delete an asset (from COS + ledger)
-app.post('/deleteDigitalAsset', async (req, res) => {
+app.post('/deleteDigitalAsset', async(req, res) => {
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     let response = await network.deleteDigitalAsset(networkObj, req.body.assetId, req.body.emailAddress);
     res.send(response);
 });
 
 //update an existing asset (replacing the file)
-app.post('/updateDigitalAsset', async (req, res) => {
+app.post('/updateDigitalAsset', async(req, res) => {
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     let response = await network.updateDigitalAsset(networkObj, req.body.digitalAssetFileName, req.body.digitalAssetFileType, req.body.digitalAssetFileBuffer, req.body.emailAddress);
     res.send(response.toString());
 });
 
 //change ownership of an asset
-app.post('/changeOwnershipOfAsset', async (req, res) => {
+app.post('/changeOwnershipOfAsset', async(req, res) => {
     let networkObj = await network.connectToNetwork(req.body.assetModifier);
     let response = await network.changeOwnershipOfAsset(networkObj, req.body.assetId, req.body.assetModifier, req.body.newAssetOwner);
     res.send(response.toString());
 });
 
 //process asset modification request
-app.post('/processAssetModRequest', async (req, res) => {
+app.post('/processAssetModRequest', async(req, res) => {
     let networkObj = await network.connectToNetwork(req.body.emailAddress);
     let response = await network.processAssetModRequest(networkObj, req.body.assetId, req.body.assetModId, req.body.emailAddress, req.body.approve, req.body.addApprovedUser);
     res.send(response.toString());
 });
 
-app.post('/getHistoryForDigitalAsset', async (req, res) => {
+app.post('/getHistoryForDigitalAsset', async(req, res) => {
     let response = await network.getHistoryForDigitalAsset(req.body.assetId);
     res.send(response.toString());
 });
