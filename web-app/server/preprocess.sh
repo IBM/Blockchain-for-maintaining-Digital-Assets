927e2a2d8fd7cccbc16f82ec245718940485a2fc
Jorge D. Rodriguez <jorgedr@us.ibm.com>
2020-08-12 11:47:06 -0400

    Fix indentation ingress and error check subdoamin

--- web-app/server/preprocess.sh
+++ web-app/server/preprocess.sh
@@ -5,6 +5,10 @@
 # Get ingress subdomain for cluster 
 INGRESS_SUBDOMAIN=$(ibmcloud ks cluster get --cluster ${PIPELINE_KUBERNETES_CLUSTER_NAME} | grep 'Ingress Subdomain:' | awk '{ print $3 } ' )
 
+# Abort if ingress subdomain not found
+[ -z "$INGRESS_SUBDOMAIN" ] && echo "Ingress subdomain for cluster not found.  Exiting " && exit 1
+
+
 # Set ingress.host value for Helm 
 API_HOST_HELM_ARGS=" --set ingress.host=${INGRESS_SUBDOMAIN}" 
 
