1895792500a070b0595e12d2d41c51d58440a57e
bmos299 <barry_moz@yahoo.com>
2020-08-31 15:03:56 -0400

    Update App.vue
    resetting to factory settings.

--- web-app/client/src/App.vue
+++ web-app/client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="10" height="10">    
+    <img alt="Vue logo" src="./assets/logo.png">    
       <h4 v-if=$route.params.emailaddress>Logged in as {{$route.params.emailaddress}}.
       <router-link to="/">Logout?</router-link>&nbsp;</h4>
       <router-view></router-view>
